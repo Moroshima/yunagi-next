a327dac37d9941d56ada0d6d057ffc0a19072f7e
Moroshima <moroshimaitsuki@gmail.com>
2024-05-07 00:43:02 +0800

    start migrating to app router

--- src/components/table.tsx
+++
@@ -1,9 +0,0 @@
-function Table({ children, ...props }: any) {
-  return (
-    <div className="table">
-      <table {...props}>{children}</table>
-    </div>
-  );
-}
-
-export default Table;
--- src/utils/feed/site.ts
+++
@@ -1,5 +0,0 @@
-export default function getSite() {
-  return process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.SITE_URL ?? "https://example.com";
-}
